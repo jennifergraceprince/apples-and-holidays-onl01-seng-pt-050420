1e00dd991a34da1782ff627ed372a46979283b3b
Jennifer Prince <jennifergraceprince@gmail.com>
2020-05-17 23:40:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,7 +20,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
 
-second_supply_for_fourth_of_july(holiday_supplies)
+#second_supply_for_fourth_of_july(holiday_supplies)
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter].each do |holiday, decorations|
